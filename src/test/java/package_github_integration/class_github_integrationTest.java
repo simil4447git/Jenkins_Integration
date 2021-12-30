6a178b849b3cfc7209d49f15fbbb19391300f631
simil4447git <96583455+simil4447git@users.noreply.github.com>
2021-12-30 13:34:35 +0530

    Add files via upload

---
+++ src/test/java/package_github_integration/class_github_integrationTest.java
@@ -0,0 +1,33 @@
+package package_github_integration;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class class_github_integrationTest {
+
+  @Test
+  public void SeleniumTestTest() {
+	  WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		
+		driver.findElement(By.id("btnLogin")).click();
+		
+		assertTrue(true);
+		
+		driver.close();
+  }
+}
